2b5057d4328dc1578612b5fdc63fc1dee173c1ac
Josh Bolland <jjm.bolland@gmail.com>
2018-12-07 14:53:17 +0000

    missing end in create operation

--- app/concepts/campaign/operation/create.rb
+++ app/concepts/campaign/operation/create.rb
@@ -1,34 +1,35 @@
 class Campaign < ActiveRecord::Base
-class Create < Trailblazer::Operation
-  extend Contract::DSL
+  class Create < Trailblazer::Operation
+    extend Contract::DSL
 
-  contract do
-    property :title
-    property :start_date
-    property :budget
-    property :brief
+    contract do
+      property :title
+      property :start_date
+      property :budget
+      property :brief
 
-    validates :title, presence: true
-    validates :start_date, presence: true, in_future: true
-    validates :budget, presence: true
-    validates :brief, presence: true
-  end
+      validates :title, presence: true
+      validates :start_date, presence: true, in_future: true
+      validates :budget, presence: true
+      validates :brief, presence: true
+    end
 
-  step Model(Campaign, :new)
-  step Contract::Build()
-  step Contract::Validate(key: :campaign)
-  step :persist!
-  step :notify!
+    step Model(Campaign, :new)
+    step Contract::Build()
+    step Contract::Validate(key: :campaign)
+    step :persist!
+    step :notify!
 
-  def authorize!(options, current_user:, **)
-    current_user.signed_in?
-  end
+    def authorize!(options, current_user:, **)
+      current_user.signed_in?
+    end
 
-  def persist!(options, params:, model:, **)
-    model.save
-  end
+    def persist!(options, params:, model:, **)
+      model.save
+    end
 
-  def notify!(options, current_user:, model:, **)
-    Campaign::Notification.(current_user, model)
+    def notify!(options, current_user:, model:, **)
+      Campaign::Notification.(current_user, model)
+    end
   end
 end
